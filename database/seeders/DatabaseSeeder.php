33dbc46d5cd2e92323015ce63c00e37996e0f77b
F14NDJ1 <firmand3@gmail.com>
2023-08-05 21:39:37 +0700

    update:update migrations file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Country;
+use App\Models\City;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +19,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Country::create(['name' => 'United Kingdom']);
+        Country::create(['name' => 'Indonesia']);
+
+        City::create(['name' => 'London', 'country_id' => 1]);
+        City::create(['name' => 'Liverpool', 'country_id' => 1]);
+        City::create(['name' => 'Leicester', 'country_id' => 1]);
+        City::create(['name' => 'Jakarta', 'country_id' => 2]);
+        City::create(['name' => 'Bandung', 'country_id' => 2]);
+        City::create(['name' => 'Surabaya', 'country_id' => 2]);
+
+        Tag::create(['name' => 'PHP', 'slug' => 'php']);
+        Tag::create(['name' => 'Java', 'slug' => 'java']);
+        Tag::create(['name' => 'Python', 'slug' => 'python']);
     }
 }
