88c9f9e677cb56b86d7ce696dd45ddae86732322
F14NDJ1 <firmand3@gmail.com>
2023-08-06 07:24:27 +0700

    add:add event and gallery controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GalleryController;
+use App\Models\Gallery;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +29,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/events', EventController::class);
+    Route::resource('/galleries', GalleryController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
